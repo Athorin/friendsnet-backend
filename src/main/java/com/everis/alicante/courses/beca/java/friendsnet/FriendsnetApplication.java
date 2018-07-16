945305bcf6903d0ad93c28a866193b27e58d0082
Pakychoko <40206025+Pakychoko@users.noreply.github.com>
2018-07-16 15:26:27 +0200

    Add files via upload

---
+++ src/main/java/com/everis/alicante/courses/beca/java/friendsnet/FriendsnetApplication.java
@@ -0,0 +1,12 @@
+package com.everis.alicante.courses.beca.java.friendsnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendsnetApplication.class, args);
+	}
+}
---
+++ src/test/java/com/everis/alicante/courses/beca/java/friendsnet/FriendsnetApplicationTests.java
@@ -0,0 +1,16 @@
+package com.everis.alicante.courses.beca.java.friendsnet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FriendsnetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
