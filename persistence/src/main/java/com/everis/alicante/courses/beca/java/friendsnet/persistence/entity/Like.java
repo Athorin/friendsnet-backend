1f13308d82d9b21777cd1f7d503ab4037bb1b6e0
Pakychoko <pakychoko@outlook.com>
2018-07-18 16:51:53 +0200

    v0.8 Application module added, maven reestructured

--- persistence/src/main/java/com/everis/alicante/courses/beca/java/friendsnet/persistence/entity/Like.java
+++ persistence/src/main/java/com/everis/alicante/courses/beca/java/friendsnet/persistence/entity/Like.java
@@ -4,7 +4,6 @@
 package com.everis.alicante.courses.beca.java.friendsnet.persistence.entity;
 
 import java.util.Date;
-import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -13,7 +12,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.everis.alicante.courses.beca.java.friendsnet.persistence.entity.enumType.LikeType;
